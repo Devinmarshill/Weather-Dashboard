40551977279315d7edcfbd6e7448271f7164fd1d
Devin <devinhill2911@gmail.com>
2023-07-27 22:24:55 -0400

    functions and info

--- script.js
+++ script.js
@@ -12,4 +12,27 @@ async function getWeatherData(city) {
     }
   }
 
-  
+  // the following function will update the UI with the weather data
+  function updateWeatherUI(data) {
+    if (!data) {
+      // Handle error when data is null such as an invalid city search
+      return;
+    }
+
+    // the following identifiers will update the current weather when the city is searched for
+    document.getElementById('title').innerText = `${data.name} (${dayjs.unix(data.dt).format('M/D/YYYY')})`;
+    document.getElementById('temp').innerText = `Temp: ${data.main.temp}°F`;
+    document.getElementById('wind').innerText = `Wind: ${data.wind.speed} MPH`;
+    document.getElementById('humidity').innerText = `Humidity: ${data.main.humidity}%`;
+  }
+
+  //the following function will get the information when the search button click event happens
+  document.getElementById('searchButton').addEventListener('click', async () => {
+    var cityInput = document.getElementById('cityInput').value.trim();
+
+    if (cityInput === '') {
+      
+      return;
+    }
+
+    
